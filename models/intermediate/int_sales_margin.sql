62acf076d1b6b1754b3cd28687db02a947762e32
Bayan Alharbi <bayanahmadalharbi@gmail.com>
2024-03-06 18:08:50 +0000

    g

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ SELECT
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64), 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
-FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p 
-    USING (products_id)
+    CAST(p.purchase_price AS float64) AS purchase_price, 
+    ROUND(s.quantity*CAST(p.purchase_price AS float64),2) AS purchase_cost,
+    ROUND(s.revenue - s.quantity*CAST(p.purchase_price AS float64),0) AS margin
+FROM {{ ref('stg_raw__raw_gz_sales') }} s
+LEFT JOIN {{ ref('stg_raw__raw_gz_product') }} p 
+    USING (products_id)
